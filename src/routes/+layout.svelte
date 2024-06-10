2229e830b5bb91c7c3b89095a951f5c38501e348
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-06-10 12:33:05 +0500

    added svelte query client config.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import "../app.css";
+  import type { PageData } from "./$types";
+  import { QueryClientProvider } from "@tanstack/svelte-query";
+
+  export let data: PageData;
+</script>
+
+<QueryClientProvider client={data.queryClient}>
+  <slot />
+</QueryClientProvider>
